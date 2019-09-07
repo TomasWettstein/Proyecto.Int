cfc67e9bcb025f0328faab1e1e81c4183568f14f
TomasWettstein <tomas_fw@hotmail.com>
2019-09-07 12:35:52 -0300

    prueba romper

--- nav.php
+++ nav.php
@@ -15,7 +15,7 @@
             <nav class="rounded-pill navbar">
               <ul class="nav nav-pills nav-fill">
                     <li class="nav-item col-lg-2">
-                            <img class="d-block img-thumbnail w-100"  src="img/dubium 2.png" alt="logo">
+                            
                     </li>
                    <li class="nav-item">
                         <a class="nav-link rounded-pill " href="index.html" tabindex="-1" aria-disabled="true">Home </a>
