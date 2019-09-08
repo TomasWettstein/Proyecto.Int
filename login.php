3178d76498ebed6f02cd87eeaf08ca6dea0a2e5b
TomasWettstein <tomas_fw@hotmail.com>
2019-09-08 16:43:23 -0300

    Añadi registro y login

--- login.php
+++ login.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="css/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css ">
-    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-    <title>Maqueta</title>
-</head>
+<?php 
+ include_once('head.php');
+?>
 
 <body>
     <section class="container">
@@ -33,3 +24,4 @@
             <center><button class="boton"><h4>Login</h4></button></center>
 
         </section>
+        
