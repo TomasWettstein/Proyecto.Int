68a586d7f68f5ac079f93fca78fbc7d3c0684755
Agustina Maraz <agus_maraz@hotmail.com>
2019-09-08 17:33:43 -0300

    footer actualizado

---
+++  crea.php
@@ -0,0 +1,14 @@
+<?php
+include_once('head.php');
+?>
+
+<body>
+    <section class="container">
+        <header>
+            <?php
+            include_once('nav.php');
+            ?>
+        </header>
+    </section>
+
+</body>
