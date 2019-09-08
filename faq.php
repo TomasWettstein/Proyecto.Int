cc8bf028deed0d6f82e6add5fd57cc10dcf4271f
Mauri <mofalius@gmail.com>
2019-09-08 16:47:53 -0300

    Mauri, cambios css: Media Query

--- faq.php
+++ faq.php
@@ -4,13 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css\style.css">
     <title>F.A.Q</title>
 </head>
 <body>
-    <div class="container">
+    <section class="container">
         <header>
-            
-        </header>
+            <?php 
+                include_once 'nav.php';
+            ?>
+        </section>
+        <article>
+            <h1 class="FAQ--titulo">PREGUNTAS <br> FRECUENTES</h1>
+        </article>
+        <article>
+            <h2 class="FAQ--1erPregunta">
+                <strong class='FAQ--du'>DU</strong><strong class = 'FAQ--bi'>BI</strong><strong class = 'FAQ--um'>UM</strong>
+            </h2>
+            <p class="FAQ--1erInfo">
+                Hola, somos el equipo de DUBIUM un juego que se trata de responder preguntas y derrotar a tus amigos en un duelo de conocimineto.
+            </p>
+        </article>
+        <article>
+            <h2 class="FAQ--2doInfo">
+                <strong class = 'FAQ--reglas'>Info</strong>
+                <strong class = 'FAQ--del'>del</strong> <strong class = 'FAQ--juego'>juego</strong>
+            </h2>
+            <p class = 'FAQ-2doInfo'>
+                El Juego se base en tres modos de juego diferentes, el modo 'Temas' un modo para jugar por las categorias que el jugador elija. El modo 'Ahorcado' que se tiene que responder una pregunta con el estilo de juego del clasico ahoracado. Y por ultimo el modo 'Muerte súbita' que se mezclan preguntas de cada categoria y solo se tiene una vida para responder la mayor cantidad de preguntas que puedas.
+            </p>
+        </article>
     </div>
 </body>
 </html>
