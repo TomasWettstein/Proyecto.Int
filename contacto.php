a65372baafec277f340cb296f9733073b69fe6a8
TomasWettstein <tomas_fw@hotmail.com>
2019-09-14 09:25:02 -0300

    Anadi seccion de contacto con sus respectivas clases y estilos

---
+++ contacto.php
@@ -0,0 +1,48 @@
+<?php
+$titulo = "Contacto";
+include_once('head.php');
+?>
+
+<body>
+
+    <body>
+        <section class="container">
+            <header>
+                <?php
+                include_once('nav.php');
+                ?>
+            </header>
+            <section class="sec--contacto">
+                <p class="p--contacto">Nosotros
+                </p>
+                <div class="caja--contacto">
+                    <p class="p--caja">Somos un grupo formado en Digital House, en el curso presencial de desarrollo web full stack, turno tarde. <br>
+                        Información del Scrum Team: <br> Agustina = Scrum Master <br> Mauricio = Parte del scrum team <br> Tomas Wettstein = Parte del scrum team. <br> Desde ya pueden dirigirse al pie de nuestra página y contactarnos atraves de la redes sociales, Facebook, Twitter o Instagram</p>
+
+
+
+                </div>
+                <div class="caja--contactenos">
+                    <p class="p_contactenos">Contáctenos</p>
+                    <p class="p--contactenos">Email</p>
+                    <p class="p--contactenos"> Dudibum@gmail.com</p>
+
+                </div>
+                <div class="c--iconos">
+                    <article class="art--redes">
+                        <ul>
+                            <li><a href="#" target="_blank"><img src="img\Iconos y cosas varias/c-facebook" alt=""></a></li>
+                            <li><a href="#" target="_blank"><img src="img\Iconos y cosas varias/c-twitter" alt=""></a></li>
+                            <li><a href="#" target="_blank"><img src="img\Iconos y cosas varias/c-insta" alt=""></a></li>
+                            <li><a href="#" target="_blank"><img src="img\Iconos y cosas varias/c-mensaje" alt=""></a></li>
+                        </ul>
+                    </article>
+                </div>
+            </section>
+            <?php
+            include_once("footer.php");
+            ?>
+
+    </body>
+
+    </html>
