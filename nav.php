c1f55e3fa9f24b564bf255a01b867bd2352a26df
Agustina Maraz <agus_maraz@hotmail.com>
2019-09-20 19:39:51 -0300

    Navbar

--- nav.php
+++ nav.php
@@ -1,13 +1,11 @@
 <nav class="rounded-pill navbar">
-    <button class="navbar-toggler bg-info btn-lg d-lg-none" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-
+    <button class="navbar-toggler bg-info btn-lg d-lg-none" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
         Menú
     </button>
-    <div class="collapse navbar-collapse d-lg-flex justify-content-end d-none " id="navbarNav">
-
+    <div class="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarNav">
         <ul class="nav nav-pills nav-fill">
             <li class="nav-item col-lg-2">
-                <a href="juegos.php"><img class="d-block  w-50" src="img\Iconos-Cosas-Varias\Logo mini.png" alt="logo"></a>
+                <a href="juegos.php"><img class="d-lg-block w-50 d-none" src="img\Iconos-Cosas-Varias\Logo mini.png" alt="logo"></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link rounded-pill " href="creaPreguntas.php"> Crea</a>
