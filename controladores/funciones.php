e08b498fe64d9ffd8a093d43d0bd27f0bfc85bb4
Agustina Maraz <agus_maraz@hotmail.com>
2019-09-26 19:30:28 -0300

    funciones

--- controladores/funciones.php
+++ controladores/funciones.php
@@ -1,4 +1,66 @@
 <?php
+session_start();
+//Funcion para validar registro
+function validarRegistro($datos){
+    $errores = [];
+    $userName = trim($datos['userName']);
+    if (empty($userName)) {
+        $errores['userName'] = "El nombre de usuario no puede estar vacio";
+    }
+    $email = trim($datos['email']);
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errores['email'] = "Email invalido";
+    }
+$password = trim($datos['password']);
+if (empty($password)) {
+    $errores['password'] = "La contraseña no puede estar vacia.";
+} elseif (!is_numeric($password) && !is_string($password)) {
+    $errores['password'] = "La contraseña deve tener letras y numeros";
+} elseif (strlen($password) < 6) {
+    $errores['password'] = "El password como mínimo debe tener 6 caracteres...";
+}
+$passwordRepeat = trim($datos['passwordRepeat']);
+if ($password != $passwordRepeat) {
+    $errores['passwordRepeat'] = "Las contraseñas deben ser iguales";
+}
+if($_FILES['avatar']['error']!=0){
+    $errores['file'] = 'Debe subir un archivo';
+}
+return $errores;
+
+}
+function nextId(){
+    $usuarioJson = file_get_contents("usuario.json");
+    $usuarios = json_decode($usuarioJson,true);
+    if(!$usuarios){
+        return 1;
+    }else {
+        $ultimoUsuario = arraypop($usuarios['usuarios']);
+        $ultimoId = $ultimoUsuario['id'];
+        $siguienteId = $ultimoId + 1;
+        return $siguienteId;
+    }
+
+}
+function crearRegistro($datos){
+    $usuario = [
+        'id' => nextId(),
+        'userName' => $datos['userName'],
+        'email' => $datos['email'],
+        'password' => password_hash($datos['password'],PASSWORD_DEFAULT),
+        'perfil' => 1
+    ];
+    return $usuario;
+
+}
+function guardarUsuario($usuario){
+    $usuarioJson = file_get_contents("usuario.json");
+    $usuarios = json_decode($usuarioJson,true);
+    $usuarios['usuarios'][] = $usuario;
+    $usuarioJson = json_encode($usuarios,JSON_PRETTY_PRINT);
+    file_put_contents('usuario.json', $usuarioJson);
+}
+
 
 function validarLogin($datos){
     $errores = [];
@@ -16,7 +78,19 @@ function validarLogin($datos){
                 $errores['password'] = 'La contraseña es incorrecta';
             }
         }
-    return $errores;
+    
+return $errores;
+}
+
+function buscarPorEmail($email){
+    $archivoJson = file_get_contents('usuario.json');
+    $array = json_decode($archivoJson, true);
+    $usuarios = $array['usuarios'];
+    foreach ($usuarios as $value) {
+         if($value['email'] === $email);
+         return $usuarios;
+    }
+    return null;
 }
 
 
--- registro.php
+++ registro.php
@@ -1,14 +1,13 @@
-
 <?php
 require_once('controladores/funciones.php');
 
 $titulo = "Registro";
 include_once('head.php');
-if($_POST) {
+if ($_POST) {
     $errores = validarRegistro($_POST);
-    if (!$errores){
+    if (!$errores) {
         $ext = pathinfo($_FILES['archivo']['name'], PATHINFO_EXTENSION);
-        move_uploaded_file($_FILES['archivo']['tmp_name'],'/avatars/' . $_POST['userName']. "." . $ext);
+        move_uploaded_file($_FILES['archivo']['tmp_name'], '/avatars/' . $_POST['userName'] . "." . $ext);
 
         $registro = crearRegistro($_POST, $avatar);
         guardarUsuario($registro);
@@ -17,8 +16,6 @@ if($_POST) {
     }
 }
 ?>
-
-
 <body>
     <section class="container">
         <header>
@@ -27,53 +24,63 @@ if($_POST) {
             ?>
         </header>
         <section class="sec--regis">
-            <p class = "p--registro">Registro</p>
-<<<<<<< HEAD
+            <p class="p--registro">Registro</p>
             <?php if (isset($errores)) : ?>
-                        <ul>
-                            <?php foreach ($errores as $value) : ?>
-                                <li><?= $value; ?></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    <?php endif; ?>
-        <form  action="" method="POST" class="form--regis">
-=======
-        <form  action="login.php" method="POST" class="form--regis" enctype="multipart/form-data">
->>>>>>> 6096ef061c506c9b213f2183848588728e086a28
-             <div class="form">
-                 <label  for="text"><p class="sub--texto">Usuario</p></label>
-                  <input class="in--regis" type="text" id="usuario" name="userName"  placeholder="Ingrese nombre de usuario" value="">
-            </div>
-            <div class="form">
-                 <label for="file"><p class="sub--texto"> Avatar </p></label>
-                 <input type="file" name="archivo" class="input--avatar">
-           
-            </div>
-            <div class="form">
-                <label  for="email"><p class="sub--texto">Email</p></label>
-                <input class="in--regis" name="email"  type="email"  id="email" placeholder= "Introducir Email" value="">
-             </div>
-             <div class="form">
-                 <label  for="password"><p class="sub--texto">Contraseña</p></label>
-                 <input class="in--regis" name="password"  type="password" id= "password" placeholder="Intruduzca una contraseña">
-            </div>
-            <div class="form">
-                <label   for="password"><p class="sub--texto">Repetir contraseña</p></label>
-                <input class="in--regis" name="passwordRepeat" type="password" class="form-control" id= "passwordRepeat" placeholder="Repita la contraseña">
-            </div>
+                <ul>
+                    <?php foreach ($errores as $value) : ?>
+                        <li><?= $value; ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+            <form action="" method="POST" class="form--regis">
+                <div class="form">
+                    <label for="text">
+                        <p class="sub--texto">Usuario</p>
+                    </label>
+                    <input class="in--regis" type="text" id="usuario" name="userName" placeholder="Ingrese nombre de usuario" value="">
+                </div>
+                <div class="form">
+                    <label for="file">
+                        <p class="sub--texto"> Avatar </p>
+                    </label>
+                    <input type="file" name="archivo" class="input--avatar">
 
-           <button  class="boton" type="submit"><p class = "p--boton"> Registrarse</p></button>
-         </form>
-         <p class="p--usuario"> Ya tenes usuario? </p>
-         <button onclick="location.href='login.php'" class="b--login" type="submit"><p class="p--registrarse">Ingresa</p></button>
+                </div>
+                <div class="form">
+                    <label for="email">
+                        <p class="sub--texto">Email</p>
+                    </label>
+                    <input class="in--regis" name="email" type="email" id="email" placeholder="Introducir Email" value="">
+                </div>
+                <div class="form">
+                    <label for="password">
+                        <p class="sub--texto">Contraseña</p>
+                    </label>
+                    <input class="in--regis" name="password" type="password" id="password" placeholder="Intruduzca una contraseña">
+                </div>
+                <div class="form">
+                    <label for="password">
+                        <p class="sub--texto">Repetir contraseña</p>
+                    </label>
+                    <input class="in--regis" name="passwordRepeat" type="password" class="form-control" id="passwordRepeat" placeholder="Repita la contraseña">
+                </div>
 
-          
-         </section>
+                <button class="boton" type="submit">
+                    <p class="p--boton"> Registrarse</p>
+                </button>
+            </form>
+            <p class="p--usuario"> Ya tenes usuario? </p>
+            <button onclick="location.href='login.php'" class="b--login" type="submit">
+                <p class="p--registrarse">Ingresa</p>
+            </button>
 
-         <?php 
-         include_once ("footer.php");
 
-         ?>
+        </section>
+
+        <?php
+        include_once("footer.php");
+
+        ?>
 
 </body>
 
