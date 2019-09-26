9a25e171d7691155b78c50aa5b74d70530238201
TomasWettstein <tomas_fw@hotmail.com>
2019-09-26 17:45:05 -0300

    funciones validar registro

--- controladores/funciones.php
+++ controladores/funciones.php
@@ -1,5 +1,62 @@
 <?php
+session_start();
+//Funcion para validar registro
+function validarRegistro($datos){
+    $errores = [];
+    $userName = trim($datos['userName']);
+    if (empty($userName)) {
+        $errores['userName'] = "El nombre de usuario no puede estar vacio";
+    }
+    $email = trim($datos['email']);
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errores['email'] = "Email invalido";
+    }
+$password = trim($datos['password']);
+if (empty($password)) {
+    $errores['password'] = "La contraseña no puede estar vacia.";
+} elseif (!is_numeric($password) && !is_string($password)) {
+    $errores['password'] = "La contraseña deve tener letras y numeros";
+} elseif (strlen($password) < 6) {
+    $errores['password'] = "El password como mínimo debe tener 6 caracteres...";
+}
+$passwordRepeat = trim($datos['passwordRepeat']);
+if ($password != $passwordRepeat) {
+    $errores['passwordRepeat'] = "Las contraseñas deben ser iguales";
+}
+if($_FILES['avatar']['error']!=0){
+    $errores['file'] = 'Debe subir un archivo';
+}
+return $errores;
 
+}
+function nextId(){
+    $usuarioJson = file_get_contents("usuario.json");
+    $usuarios = json_decode($usuarioJson,true);
+    if(!$usuarios){
+        return 1;
+    }else {
+        $ultimoUsuario = arraypop($usuarios['usuarios']);
+        $ultimoId = $ultimoUsuario['id'];
+        $siguienteId = $ultimoId + 1;
+        return $siguienteId;
+    }
 
+}
+function crearRegistro($datos){
+    $usuario = [
+        'id' => nextId(),
+        'userName' => $datos['userName'],
+        'email' => $datos['email'],
+        'password' => password_hash($datos['password'],PASSWORD_DEFAULT),
+        'perfil' => 1
+    ];
+    return $usuario;
 
-?>
+}
+function guardarUsuario($usuario){
+    $usuarioJson = file_get_contents("usuario.json");
+    $usuarios = json_decode($usuarioJson,true);
+    $usuarios['usuarios'][] = $usuario;
+    $usuarioJson = json_encode($usuarios,JSON_PRETTY_PRINT);
+    file_put_contents('usuario.json', $usuarioJson);
+}
--- registro.php
+++ registro.php
@@ -1,7 +1,23 @@
+
 <?php
+require_once('controladores/funciones.php');
+
 $titulo = "Registro";
 include_once('head.php');
+if($_POST) {
+    $errores = validarRegistro($_POST);
+    if (!$errores){
+        $ext = pathinfo($_FILES['archivo']['name'], PATHINFO_EXTENSION);
+        move_uploaded_file($_FILES['archivo']['tmp_name'],'/avatars/' . $_POST['userName']. "." . $ext);
+
+        $registro = crearRegistro($_POST, $avatar);
+        guardarUsuario($registro);
+        header('location:login.php');
+        exit;
+    }
+}
 ?>
+
 
 <body>
     <section class="container">
@@ -12,22 +28,29 @@ include_once('head.php');
         </header>
         <section class="sec--regis">
             <p class = "p--registro">Registro</p>
-        <form  action="login.php" method="POST" class="form--regis">
+            <?php if (isset($errores)) : ?>
+                        <ul>
+                            <?php foreach ($errores as $value) : ?>
+                                <li><?= $value; ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
+        <form  action="" method="POST" class="form--regis">
              <div class="form">
                  <label  for="text"><p class="sub--texto">Usuario</p></label>
-                  <input class="in--regis" type="text" id="usuario"  placeholder="Ingrese nombre de usuario">
+                  <input class="in--regis" type="text" id="usuario" name="userName"  placeholder="Ingrese nombre de usuario" value="">
             </div>
             <div class="form">
                 <label  for="email"><p class="sub--texto">Email</p></label>
-                <input class="in--regis"  type="email"  id="email" placeholder= "Introducir Email">
+                <input class="in--regis" name="email"  type="email"  id="email" placeholder= "Introducir Email" value="">
              </div>
              <div class="form">
-                 <label  for="Password"><p class="sub--texto">Contraseña</p></label>
-                 <input class="in--regis"  type="password" id= "password" placeholder="Intruduzca una contraseña">
+                 <label  for="password"><p class="sub--texto">Contraseña</p></label>
+                 <input class="in--regis" name="password"  type="password" id= "password" placeholder="Intruduzca una contraseña">
             </div>
             <div class="form">
-                <label   for="Password"><p class="sub--texto">Repetir contraseña</p></label>
-                <input class="in--regis"  type="password" class="form-control" id= "password" placeholder="Repita la contraseña">
+                <label   for="password"><p class="sub--texto">Repetir contraseña</p></label>
+                <input class="in--regis" name="passwordRepeat" type="password" class="form-control" id= "passwordRepeat" placeholder="Repita la contraseña">
             </div>
 
            <center><button  class="boton" type="submit"><p class = "p--boton"> Registrarse</p></button></center> 
